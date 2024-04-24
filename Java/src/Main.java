5e684abffc984eb59751b3872660d0cc6a54b9b3
Angelo <gabrie.santos@cest.edu.br>
2024-04-24 16:02:01 -0300

    singleton, adapter, Observer

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,6 +1,6 @@
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Repositorio de estudos em java");
-        }
-    }
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Repositorio de estudos em java");
+//        }
+//    }
