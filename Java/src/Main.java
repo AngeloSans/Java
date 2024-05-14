1088bb10cd4a44a25bc623c1591a0e6a9fb2a20d
Angelo Santos <gabrielange883@gmail.com>
2024-05-14 10:40:38 -0300

    Reflection

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,6 +1,10 @@
-
+//import Especial.Cat;
+//
+//import java.lang.reflect.Field;
+//
 //public class Main {
 //    public static void main(String[] args) {
-//        System.out.println("Repositorio de estudos em java");
-//        }
+//        System.out.println("KEEP IMPROVING YOURSELF");
+//
 //    }
+//}
