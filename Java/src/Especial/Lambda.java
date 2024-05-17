0e0043b8fd2a4de2bb967450b0d75d493fda8c4d
Angelo Santos <gabrielange883@gmail.com>
2024-05-17 13:44:18 -0300

    Lambda

---
+++ Java/src/Especial/Lambda.java
@@ -0,0 +1,16 @@
+//package Especial;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//public class Lambda {
+//    public static void main(String[] args) {
+//        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+//        List<Integer> quadrados = numbers.stream()
+//                .map(n -> n * n)
+//                .collect(Collectors.toList());
+//        System.out.println(quadrados);
+//    }
+//}
