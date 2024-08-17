8ec9ad6a56eddf1f2b9959a2478afc849f6095b1
Angelo Santos <gabrielange883@gmail.com>
2024-08-17 08:26:41 -0300

    Threads

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,10 +1,12 @@
-//
-//
-//import java.lang.reflect.Field;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("KEEP IMPROVING YOURSELF");
-//
-//    }
-//}
+/*
+
+import java.lang.reflect.Field;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("KEEP IMPROVING YOURSELF");
+
+   }
+}
+
+ */
