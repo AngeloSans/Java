e9772ea41eee85851993074a9aae42c12322da9f
Angelo Santos <gabrielange883@gmail.com>
2024-08-18 08:09:51 -0300

    Threads

--- Java/src/Threads/Threads.java
+++ Java/src/Threads/Threads.java
@@ -1,5 +1,24 @@
 package Threads;
 
 public class Threads {
-
+    public static void main(String[] args) {
+            new Thread(p1).start();
+            new Thread(p2).start();
+    }
+    private static Runnable p1 = new Runnable() {
+        @Override
+        public void run() {
+            for(int i = 0; i < 10; i++){
+                System.out.println("Thread 1 : " + i);
+            }
+        }
+    };
+    private static Runnable p2 = new Runnable() {
+        @Override
+        public void run() {
+            for(int j = 0; j < 10; j++){
+                System.out.println("Thread 2 : " + j);
+            }
+        }
+    };
 }
