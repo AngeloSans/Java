112e5537eecf49f45cf629b5f20fe00e002384ef
Angelo Santos <gabrielange883@gmail.com>
2024-08-18 09:00:49 -0300

    Threads Syncronized

--- Java/src/Threads/Threads.java
+++ Java/src/Threads/Threads.java
@@ -1,24 +1,45 @@
 package Threads;
 
 public class Threads {
+    private static final Object lock = new Object();
+
     public static void main(String[] args) {
-            new Thread(p1).start();
-            new Thread(p2).start();
+        new Thread(p1).start();
+        new Thread(p2).start();
     }
+
     private static Runnable p1 = new Runnable() {
         @Override
         public void run() {
-            for(int i = 0; i < 10; i++){
-                System.out.println("Thread 1 : " + i);
+            synchronized (lock) {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Thread 1 : " + i);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
+
         }
     };
     private static Runnable p2 = new Runnable() {
         @Override
         public void run() {
-            for(int j = 0; j < 10; j++){
-                System.out.println("Thread 2 : " + j);
+            synchronized (lock) {
+                for (int j = 0; j < 10; j++) {
+                    System.out.println("Thread 2 : " + j);
+                    try {
+                        Thread.sleep(50000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
+
         }
+
+
     };
 }
